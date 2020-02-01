054bd9b08b5be88461fe4035a4080bc75da99b03
Andres Contreras <andres_c8@outlook.com>
2020-02-01 13:08:03 -0500

    estructura del proyecto

---
+++ src/components/pregunta.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+
+const Pregunta = () => {
+  return (
+    <Fragment>
+      <h2>Coloca tu presupuesto</h2>
+      <form>
+        <input
+          type="number"
+          className="u-full-width"
+          placeholder="coloca tu presupesto"
+        />
+
+        <input type="submit" className="button-primary u-full-width" />
+      </form>
+    </Fragment>
+  );
+};
+
+export default Pregunta;
